0339dcd3a2cfbb9b505559e5b98fd327645bdfcf
XuJieJJ <3084966130@qq.com>
2023-08-14 22:43:24 +0800

    新增菜品管理代码实现

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/admin/common")
@@ -22,9 +26,23 @@ public class CommonController {
      */
     @PostMapping("/upload")
     @ApiOperation("文件上传")
-    public Result<String> upload(MultipartFile file){
+    public Result<String> upload(MultipartFile file)  {
         log.info("文件上传: {}",file);
+        /**
+         * TODO 目前存储位置在本地 后期更改至阿里云存储
+         */
+        //获取文件原始名字
+        String originalFilename = file.getOriginalFilename();
 
-        return null;
+        //将文件保存在本地磁盘
+        try {
+            file.transferTo((new File("E:\\STUDY\\imgs\\"+originalFilename)));
+            String path ="E:\\STUDY\\imgs\\"+originalFilename;
+            return Result.success(path);
+        } catch (IOException e) {
+            log.error("文件上传失败",e);
+        }
+
+        return  Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
